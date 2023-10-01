e8708e3d3ddafba32ac29e8c0b60f0c21e124295
Anish Shahi <ashahi322@gmail.com>
2023-10-01 09:42:31 +0545

    About Section Complete

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -11,7 +11,7 @@ export default function HeroSection() {
         <div className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 ">
-              Hello I'm {""}
+              Hello I&apos;m {""}
             </span>
             <br></br>
             <TypeAnimation
@@ -33,10 +33,10 @@ export default function HeroSection() {
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6">
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
+            industry. Lorem Ipsum has been the industry standard dummy text ever
+            since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only
+            five centuries, but also the leap into electronic typesetting,
             remaining essentially unchanged. It was popularised in the 1960s
             with the release of Letraset sheets containing Lorem Ipsum passages,
             and more recently with desktop publishing software like Aldus
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import NavBar from "./components/NavBar";
+import AboutMeSection from "./components/AboutMeSection";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <NavBar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
+        <AboutMeSection />
       </div>
     </main>
   );
